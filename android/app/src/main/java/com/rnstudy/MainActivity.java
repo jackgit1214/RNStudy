4ec01df362f22ac09adaad83a17b7c55c0649ea1
lilj <hongyullj@sohu.com>
2019-08-30 16:47:18 +0800

    first commit android

---
+++ android/app/src/main/java/com/rnstudy/MainActivity.java
@@ -0,0 +1,39 @@
+package com.rnstudy;
+
+import android.os.Bundle;
+import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
+
+// react-native-splash-screen >= 0.3.1
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+       // SplashScreen.show(this);
+
+        SplashScreen.show(this, R.style.SplashScreenTheme);
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    protected String getMainComponentName() {
+        return "myTest";
+    }
+
+//    @Override
+//    protected ReactActivityDelegate createReactActivityDelegate() {
+//        return new ReactActivityDelegate(this, getMainComponentName()) {
+//            @Override
+//            protected ReactRootView createRootView() {
+//                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+//            }
+//        };
+//    }
+}
---
+++ android/app/src/main/java/com/rnstudy/MainApplication.java
@@ -0,0 +1,55 @@
+package com.rnstudy;
+
+import android.app.Application;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import ui.popovermenu.RNPopoverMenuPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+
+            new LinearGradientPackage(),
+            new VectorIconsPackage(),
+              new RNPopoverMenuPackage(),
+            new RNGestureHandlerPackage(),
+              new SplashScreenReactPackage()  //here
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
